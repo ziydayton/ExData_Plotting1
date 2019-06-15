6151c8f7627285b93651ba20e9e00d74710cc36b
Jeff Dayton <ziydayton@hotmail.com>
2019-06-15 16:00:29 -0400

    Final draft of plot 4 code and png.

--- plot4.R
+++ plot4.R
@@ -22,18 +22,27 @@ file.remove("household_power_consumption.txt")
 rm(temp, con, fileURL) 
 
 #sort and prepare df
-df2 <- df %>% 
-  select(Sub_metering_3, Sub_metering_2, Sub_metering_1, Time, Date) %>% 
+df <- df %>% 
+  select(Global_reactive_power,
+         Sub_metering_3, Sub_metering_2, Sub_metering_1,
+         Voltage,
+         Global_active_power,
+         Time, Date) %>% 
   mutate(Date = dmy(Date)) %>% 
   filter(Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02")) %>%
   mutate(dtg = ymd_hms(paste(Date, Time))) %>% 
+  rename(GAP = Global_active_power) %>% 
+  mutate(GAP = as.numeric(GAP)) %>%
+  mutate(Voltage = as.numeric(Voltage)) %>% 
   rename(SM1 = Sub_metering_1) %>% 
   mutate(SM1 = as.numeric(SM1)) %>%
   rename(SM2 = Sub_metering_2) %>% 
   mutate(SM2 = as.numeric(SM2)) %>%
   rename(SM3 = Sub_metering_3) %>% 
   mutate(SM3 = as.numeric(SM3)) %>%
-  select(dtg, SM1, SM2, SM3)
+  rename(GRP = Global_reactive_power) %>% 
+  mutate(GRP = as.numeric(GRP)) %>%
+  select(dtg, GAP, Voltage, SM1, SM2, SM3, GRP)
 
 ##Generate plot
 #Specified png that is 480x480pixels - Although defealt; included for specificity
@@ -42,20 +51,44 @@ png(filename = "plot4.png",
     height = 480,
     units = "px")
 
-#Create plot 3
+#Set plot pallette
+par(mfrow = c(2,2))
+
+#Create 1st plot
+plot(GAP ~ dtg, 
+     data = df,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power")
+
+#Create 2nd plot
+plot(Voltage ~ dtg, 
+     data = df,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage")
+
+#Create 3rd plot
 plot(SM1 ~ dtg, 
-     data = df2,
+     data = df,
      type = "n",
      xlab = "",
      ylab = "Energy sub metering")
-lines(SM1 ~ dtg, data = df2, type = "l", col = "black")
-lines(SM2 ~ dtg, data = df2, type = "l", col = "red")
-lines(SM3 ~ dtg, data = df2, type = "l", col = "blue")
+lines(SM1 ~ dtg, data = df, type = "l", col = "black")
+lines(SM2 ~ dtg, data = df, type = "l", col = "red")
+lines(SM3 ~ dtg, data = df, type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1", 
                               "Sub_metering_2", 
                               "Sub_metering_3"), 
        lty = c(1, 1, 1), 
-       col = c("black", "red", "blue"))
+       col = c("black", "red", "blue"),
+       bty = "n")
+#Create 4th plot
+plot(GRP ~ dtg, 
+     data = df,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global_reactive_power")
 
 #Turn off png device
 dev.off()
